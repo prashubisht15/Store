a3bbaf58f7848ddd7611be8532880060cb7ef738
Prashant Singh <prashantsinghbisht710@gmail.com>
2022-12-01 19:45:35 +0530

    add filter component css

--- src/Components/Filter.js
+++ src/Components/Filter.js
@@ -4,11 +4,13 @@ const Filter = ({prodlist}) => {
   return (
     <div className='filter-section'>
       <form>
-    
         <h2>Color</h2>
         <div className='category-list'>
           <label>
-            <input type="checkbox" name=""  value={'Green'} /> <span>Green</span>
+            <input type="checkbox" name=""  value={'Red'} /> <span>Red</span>
+          </label>
+          <label>
+            <input type="checkbox" name=""  value={'Blue'} /> <span>Blue</span>
           </label>
           <label>
             <input type="checkbox" name=""  value={'Green'} /> <span>Green</span>
@@ -28,29 +30,29 @@ const Filter = ({prodlist}) => {
         <h2>Price</h2>
         <div className='category-list'>
           <label>
-            <input type="checkbox" name=""  value={'Men'} /> <span>0 - Rs250</span>
+            <input type="checkbox" name=""  value={'Men'} /> <span htmlFor="">0 - Rs250</span>
           </label>
           <label>
-            <input type="checkbox" name=""  value={'Men'} /> <span>Rs251 - Rs450</span>
+            <input type="checkbox" name=""  value={'Men'} /> <span htmlFor="">Rs251 - Rs450</span>
           </label>
           <label>
-            <input type="checkbox" name=""  value={'Men'} /> <span>Above Rs450</span>
+            <input type="checkbox" name=""  value={'Men'} /> <span htmlFor="">Above Rs450</span>
           </label>
         </div>
 
         <h2>Type</h2>
         <div className='category-list'>
           <label>
-            <input type="checkbox" name=""  value={'Men'} /> <span>Polo</span>
+            <input type="checkbox" name=""  value={'Men'} /> <span htmlFor="">Polo</span>
           </label>
           <label>
-            <input type="checkbox" name=""  value={'Men'} /> <span>Hoodie</span>
+            <input type="checkbox" name=""  value={'Men'} /> <span htmlFor="">Hoodie</span>
           </label>
           <label>
-            <input type="checkbox" name=""  value={'Green'} /> <span>Green</span>
+            <input type="checkbox" name=""  value={'Green'} /> <span htmlFor="">Green</span>
           </label>
           <label>
-            <input type="checkbox" name=""  value={'Men'} /> <span>Basic</span>
+            <input type="checkbox" name=""  value={'Men'} /> <span htmlFor="">Basic</span>
           </label>
         </div>
       </form>
